8306d191a7dd85809b016e1c925717673d01738d
jesselam00 <jesselam2000@gmail.com>
2025-07-28 23:06:00 -0500

    Add test prompt to gemini

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    resp = client.models.generate_content(
+        model='gemini-2.0-flash-001', 
+        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum?'
+    )
+    print("Prompt tokens:", resp.usage_metadata.prompt_token_count)
+    print("Response tokens: ", resp.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(resp.text)
 
 
 if __name__ == "__main__":
